99da2ac9fc3217a6bc5bcc4638ec6d008d8d15dc
Fatima Pillosu <fatima.pillosu@ecmwf.int>
2019-10-22 03:49:35 +0100

    Update GoogleMaps_LatLon.py

--- GoogleMaps_LatLon.py
+++ GoogleMaps_LatLon.py
@@ -1,1 +1,15 @@
-pip install -U googlemaps
+import googlemaps
+
+KEY_API='AIzaSyAQmw5ydRWjvpf2J9XtBETTCDV8Oz__rEs'
+
+gmaps_key = googlemaps.Client(key=KEY_API)
+
+# Geocoding an address
+geocode_result = gmaps_key.geocode('RG67PG')
+
+# Extract Lat/lon
+lat = geocode_result[0]['geometry']['location']['lat']
+lon = geocode_result[0]['geometry']['location']['lng']
+
+print(lat)
+print(lon)
